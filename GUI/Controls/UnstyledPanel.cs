c0e13008edd39f9da2feea4ed6226155171b385f
Named1ess <49092640+Named1ess@users.noreply.github.com>
2026-03-12 22:13:14 +0800

    Revert "Initial commit"
    This reverts commit a07e0874cf312fecbce50fd8a21d0c84675efb9f.

--- GUI/Controls/UnstyledPanel.cs
+++
@@ -1,7 +0,0 @@
-using System.Windows.Forms;
-
-namespace GUI.Controls;
-
-public class UnstyledPanel : Panel
-{
-}
--- ValveResourceFormat/TextureDecoders/ITextureDecoder.cs
+++
@@ -1,9 +0,0 @@
-using SkiaSharp;
-
-namespace ValveResourceFormat.TextureDecoders
-{
-    internal interface ITextureDecoder
-    {
-        public abstract void Decode(SKBitmap bitmap, Span<byte> input);
-    }
-}
